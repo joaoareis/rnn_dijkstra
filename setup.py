0ccb02e3d0b26f34bba2121988e9aaff04c20539
ucakjmq <ucakjmq@Reis-MBP.home>
2018-02-09 18:11:31 +0000

    Added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='DijkstraRNN',
+    version='0.1.0',
+    author='Joao Reis',
+    author_email='ucakjmq@ucl.ac.uk',
+    python_requires=">=3.5",
+    install_requires=[
+    "numpy",
+    "torch",
+    "jupyter",
+    "matplotlib",
+    "scipy"
+],
+)
